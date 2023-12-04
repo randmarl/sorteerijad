6cf53e1353de1625a70b85ec6a203f84a8d479d7
Marleen <75412079+randmarl@users.noreply.github.com>
2023-12-04 22:30:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function setnewimage() {
+    document.getElementById("img2").src = "https://i.ytimg.com/vi/yxFhbmTULlQ/maxresdefault.jpg";
+    }
+   function setoldimage() {
+    document.getElementById("img2").src = "https://www.coca-cola.com/content/dam/onexp/ee/et/tethered-caps/H273635_P628177_TCCC_Tethered_Caps_Estonian_LD_Desktop_1440x810.png?fbclid=IwAR0vNaT3AcvnfqpsvFKpCEp0znY8VnI6Hk69IAY1Bz_ywRUc95__n83Y3y0";
+    }
